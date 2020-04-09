6c4c21592de20150389172145c8a06a7b31b03df
MinenhleSolson <minenhlecele34@gmail.com>
2020-04-09 12:49:28 +0200

    Updates

---
+++ pages/Jaguar/F-type/pages.js
@@ -0,0 +1,19 @@
+//scripts for the car pages 
+
+//script for the image slider
+var slideIndex = 1;
+showDivs(slideIndex);
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
